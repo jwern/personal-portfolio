74552c13d1e2ff3fce0328b7b466c39132079cea
Jillian Werner <jillianlwerner@gmail.com>
2021-06-22 20:18:30 -0400

    Add dev and production build environments; move HTML to src/template.html

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: __dirname + "/src/images",
+          to: "images",
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        // Although we're not using the Asset Module in our production build,
+        // we need it for our dev server which uses JS injection of styles
+        test: /\.(jpg|svg|png|pdf|gif)$/,
+        type: "asset/resource",
+      },
+      // {
+      //   test: /\.html$/,
+      //   loader: "html-loader",
+      // },
+    ],
+  },
+};
